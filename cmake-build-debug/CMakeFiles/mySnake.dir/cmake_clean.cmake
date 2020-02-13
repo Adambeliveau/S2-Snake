a02a25ad1c83b5eaeb4fcca39dc6d8d8529629af
AdamBeliveau <bela1003@usherbrooke.ca>
2020-02-13 11:12:37 -0500

    init

---
+++ cmake-build-debug/CMakeFiles/mySnake.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/mySnake.dir/main.cpp.obj"
+  "libmySnake.dll.a"
+  "mySnake.exe"
+  "mySnake.exe.manifest"
+  "mySnake.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/mySnake.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
