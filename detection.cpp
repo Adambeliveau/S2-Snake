d1bd593695f420bad194cfdbafd12f8297c64392
AdamBeliveau <bela1003@usherbrooke.ca>
2020-02-14 09:02:21 -0500

    mange des cocombre

---
+++ detection.cpp
@@ -0,0 +1,5 @@
+//
+// Created by adamb on 2020-02-13.
+//
+
+#include "detection.h"
---
+++ detection.h
@@ -0,0 +1,16 @@
+//
+// Created by adamb on 2020-02-13.
+//
+
+#ifndef MYSNAKE_DETECTION_H
+#define MYSNAKE_DETECTION_H
+
+
+
+class detection {
+
+};
+
+
+
+#endif //MYSNAKE_DETECTION_H
